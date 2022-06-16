cc6645bcb7f6832e0c0015794472dbcea2fd40b2
jevazquezb <je.vazquezb@gmail.com>
2022-06-16 13:26:31 -0500

    Add eventListeners to index.js file

--- src/index.js
+++ src/index.js
@@ -1,15 +1,37 @@
 import './styles/reset.css';
 import './styles/style.css';
-import displayPlayer from './modules/display.js';
 
-const players = [
-  { name: 'Tom', score: 100 },
-  { name: 'Andrés', score: 20 },
-  { name: 'David', score: 50 },
-  { name: 'Dino', score: 78 },
-  { name: 'Daril', score: 125 },
-  { name: 'Jesús', score: 77 },
-  { name: 'Alfred', score: 42 },
-];
+// import newGame from './modules/newgame.js'
+import { sendScore } from './modules/api_score.js';
+import refresh from './modules/refresh.js';
 
-players.forEach((player) => displayPlayer(player));
+const gameName = 'My first game';
+const baseLink = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
+
+//newGame(gameName, baseLink);
+
+const gameId = '6OWBy4EcFFE5EWjho928';
+const scoreLink = `${baseLink}${gameId}/scores/`;
+
+const update = document.querySelector('#refresh');
+const userInput = document.querySelector('#user-input');
+const scoreInput = document.querySelector('#score-input');
+const form = document.querySelector('form');
+const addMsg = document.querySelector('#add-msg');
+
+window.addEventListener('load', () => {
+  refresh(scoreLink);
+});
+update.addEventListener('click', () => {
+  refresh(scoreLink);
+});
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const newScore = sendScore(userInput.value, +scoreInput.value, scoreLink);
+  newScore.then((msg) => {
+    addMsg.textContent = msg;
+    refresh(scoreLink);
+  });
+  e.target.reset();
+  setTimeout(() => addMsg.textContent = '', 5000);  
+});
